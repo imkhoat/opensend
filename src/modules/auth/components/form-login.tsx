35209ceb07e72b4780b9a098fe471924c631b425
imkhoat <khoatbui.idea@gmail.com>
2025-03-16 15:20:24 +0700

    feat: add nessesary components

---
+++ src/modules/auth/components/form-login.tsx
@@ -0,0 +1,7 @@
+import { Card } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+export default function FormLogin(){
+
+}
