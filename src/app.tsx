259b8556b8f2aea903d1f2d710e0960928b60265
imkhoat <khoatbui.idea@gmail.com>
2025-03-16 15:05:27 +0700

    feat: setup rtk query

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
+      <div className="font-display flex flex-col justify-center items-center h-screen mx-auto w-1/2 gap-2">
         {count}
-        <Button onClick={() => setCount((count) => count + 1)}>
+        <Button color="brand" onClick={() => setCount((count) => count + 1)}>
           Increment
         </Button>
+        <Input type="email" placeholder="Email" />
       </div>
     </>
   );
