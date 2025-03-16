9bc27e4d900f48dcc70fc0bb913c7c0478874e4c
Miguel <85888237+imkhoat@users.noreply.github.com>
2025-03-16 20:44:12 +0700

    feat: implement theme mode (#3)

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
 import LoginPage from "@/modules/auth/pages/login";
+import ModeToggle from "@/components/extends/mode-toggle";
+
 function App() {
   return (
     <main className="bg-secondary">
+      <header className="flex justify-end p-4">
+        <ModeToggle />
+      </header>
       <div className="font-display flex flex-col justify-center items-center h-screen mx-auto gap-2">
         <LoginPage />
       </div>
